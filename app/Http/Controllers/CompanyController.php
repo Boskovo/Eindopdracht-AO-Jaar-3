397b773c654ef75cebb1f3d2a0b9fe205ac70c2c
rick98788 <51737436+rick98788@users.noreply.github.com>
2022-01-14 11:16:53 +0100

    added index function to controllers

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -10,11 +10,11 @@ class CompanyController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('company.index');
     }
 
     /**
