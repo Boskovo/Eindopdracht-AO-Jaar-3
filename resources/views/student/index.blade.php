af3bcb7364104c23e9466f2945b9ecbfd2dc7fc7
MarinusGoossens <58777951+MarinusGoossens@users.noreply.github.com>
2022-01-20 11:20:20 +0100

    scss inrichten

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="main py-4">
-        <div class="row">
-            <div class="col-large-12">
-                <H1>Mijn stage</H1>
-                <p>Bedrijfsnaam</p>
-                <p>Uren Stage: 0</p>
-                <p>Alle documenten ingeleverd</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <h2>ingeleverde documenten</h2>
-                <ul>
-                    <li>Documentnaam</li>
-                    <li>Documentnaam</li>
-                    <li>Documentnaam</li>
-                </ul>
-                <div class="Buttonwrapper">
-                    <a class="btn" href="">Pok aanvragen</a>
-                    <a class="btn" href="">Document inlevern</a>
-                </div>
-              </div>
-              <div class="col">
-    
-              </div>
-        </div>
+<section class="student">
+  <div class="main py-4">
+    <div class="row">
+      <div class="col-large-12">
+        <H1>Mijn stage</H1>
+        <p>Bedrijfsnaam</p>
+        <p>Uren Stage: 0</p>
+        <p>Alle documenten ingeleverd</p>
+      </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <h2>ingeleverde documenten</h2>
+        <ul>
+          <li>Documentnaam</li>
+          <li>Documentnaam</li>
+          <li>Documentnaam</li>
+        </ul>
+        <div class="Buttonwrapper">
+          <a class="btn" href="">Pok aanvragen</a>
+          <a class="btn" href="">Document inlevern</a>
+        </div>
+      </div>
+      <div class="col">
+
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
