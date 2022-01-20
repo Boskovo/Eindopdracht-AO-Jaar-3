2040af53bc60455fa4311629c77cad654e3cf8cd
shanos123 <60260010+shanos123@users.noreply.github.com>
2022-01-20 11:48:47 +0100

    nbcdsjkncids
    inhoudt van docenten verandert

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,31 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="main py-4">
-        <div class="row">
-            <div class="col-large-12">
-                <H1>Mijn stage</H1>
-                <p>Bedrijfsnaam</p>
-                <p>Uren Stage: 0</p>
-                <p>Alle documenten ingeleverd</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col">
-                <h2>ingeleverde documenten</h2>
-                <ul>
-                    <li>Documentnaam</li>
-                    <li>Documentnaam</li>
-                    <li>Documentnaam</li>
-                </ul>
-                <div class="Buttonwrapper">
-                    <a class="btn" href="">Pok aanvragen</a>
-                    <a class="btn" href="">Document inlevern</a>
-                </div>
-              </div>
-              <div class="col">
-    
-              </div>
+    <section class="main py-4">
+
+    </section>
+
+    <section class="container2">
+      <div class="huidig-bedrijf">
+        <div class="text">
+          
         </div>
-    </div>
+        <div class="block_company">
+
+        </div>
+    
+      </div>
+    <section>
 @endsection
