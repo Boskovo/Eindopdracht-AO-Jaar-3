57405b1e2de9c5024a458d43959745f473b30961
MarinusGoossens <58777951+MarinusGoossens@users.noreply.github.com>
2022-01-21 12:16:24 +0100

    student front-end

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -5,48 +5,58 @@
   <div class="container">
     <div class="row">
       <div class="col-large-12">
-        <H1>Mijn stage</H1>
-        <p>Bedrijfsnaam</p>
-        <p>Uren Stage: 0</p>
-        <p>Alle documenten ingeleverd</p>
+        <div class="student-overall">
+          <H1>Mijn stage</H1>
+          <p>Bedrijfsnaam</p>
+          <p>Uren Stage: 0</p>
+          <p>Alle documenten ingeleverd</p>
+        </div>
       </div>
     </div>
 
     <div class="row">
       <div class="col">
-        <h2>ingeleverde documenten</h2>
-        <ul>
-          <li>Documentnaam</li>
-          <li>Documentnaam</li>
-          <li>Documentnaam</li>
-        </ul>
-        <div class="Buttonwrapper">
-          <a class="btn" href="">Pok aanvragen</a>
-          <a class="btn" href="">Document inlevern</a>
+        <div class="document-display">
+          <h2>Ingeleverde documenten</h2>
+          <ul>
+            <li>Documentnaam</li>
+            <li>Documentnaam</li>
+            <li>Documentnaam</li>
+          </ul>
+          <div class="buttonwrapper">
+            <div class="pok-button text-center">
+              <a href="">Pok aanvragen</a>
+            </div>
+            <div class="document-button text-center">
+              <a href="">Document inleveren</a>
+            </div>
+          </div>
         </div>
       </div>
       <div class="col">
-        <h2>
-          Afspraken
-        </h2>
-        <ul>
-          <li>
-            <p>Afspraaktitel</p>
-            <p>20-1-2022 / 12:00</p>
-          </li>
-          <li>
-            <p>Afspraaktitel</p>
-            <p>20-1-2022 / 12:00</p>
-          </li>
-          <li>
-            <p>Afspraaktitel</p>
-            <p>20-1-2022 / 12:00</p>
-          </li>
-          <li>
-            <p>Afspraaktitel</p>
-            <p>20-1-2022 / 12:00</p>
-          </li>
-        </ul>
+        <div class="appointment-display">
+          <h2>
+            Afspraken
+          </h2>
+          <ul>
+            <li>
+              <p>Afspraaktitel</p>
+              <p>20-1-2022 / 12:00</p>
+            </li>
+            <li>
+              <p>Afspraaktitel</p>
+              <p>20-1-2022 / 12:00</p>
+            </li>
+            <li>
+              <p>Afspraaktitel</p>
+              <p>20-1-2022 / 12:00</p>
+            </li>
+            <li>
+              <p>Afspraaktitel</p>
+              <p>20-1-2022 / 12:00</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
