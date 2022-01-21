71ae315c5e251bbbfba23b1665d3980379e02b14
shanos123 <60260010+shanos123@users.noreply.github.com>
2022-01-21 12:21:38 +0100

    front end docenten
    blocken toegevoegd en stylen

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,19 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <section class="main py-4">
+<div class="container">
+  <div class="row">
+    <div class="col-lg-5">
+        <div class="container_profiel_docent">
+          <div class="profiel_foto">
+        
+          </div>
+          <div class="info_persoon">
 
-    </section>
+          </div>
+          <div class="CV_block">
 
-    <section class="container2">
-      <div class="huidig-bedrijf">
-        <div class="text">
-          
+          </div>
+
         </div>
-        <div class="block_company">
-
-        </div>
-    
       </div>
-    <section>
+      <div class="col">
+        <div class="container_informatie_stage">
+          <div class="huidig-bedrijf">
+          <div class="text">
+
+          </div>
+          <div class="block_company">
+
+          </div>
+    
+          </div>
+        </div>
+      </div>
+  
+    <div class="row no-pad">
+      <div class="col">
+        <div class="container_ingeleverde_documenten">
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
