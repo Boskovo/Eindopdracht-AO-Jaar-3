6be60e7936959f49372763fdd4cece86f758866c
Boskovo <68953190+Boskovo@users.noreply.github.com>
2022-01-23 15:37:40 +0100

    Front-end bedrijvenpagina
    Verder gewerkt aan de front end van het bedrijvendashboard

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,30 +1,44 @@
 @extends('layouts.app')
+@section('title', 'Bedrijven')
 
 @section('content')
 <div class="container">
   <div class="row">
     <div class="col">
-      <div class="container bg-primary text-white rounded m-3">
-        <div class="row">
-          <H1 class="m-4">Stagiaires</H1>
+      <div class="card">
+        <h1 class="m-4">Stagiaires</h1>
           <div class="m-4 mt-2">
             <p>Pietje Schroot</p>
             <p>Christiaan Boldewijn</p>
             <p>Bastijn Potgrave</p>
           </div>
-        </div>
+      </div>
+      <div class="card">
+        <h1 class="m-4">Vacatures</h1>
+          <div class="m-4 mt-2">
+              <p>VacatureTitel</p>
+              <p>VacatureTitel</p>
+              <p>VacatureTitel</p>
+          </div>
+      </div>
     </div>
     <div class="col">
-    <div class="container bg-primary text-white rounded m-3">
-        <div class="row">
-          <H1 class="m-4">Vacatures</H1>
+      <div class="card">
+        <h1 class="m-4">Documenten</h1>
           <div class="m-4 mt-2">
-            <p>VacatureTitel</p>
-            <p>VacatureTitel</p>
-            <p>VacatureTitel</p>
+            <p>documentNaam</p>
+            <p>documentNaam</p>
+            <p>documentNaam</p>
           </div>
         </div>
+        <div class="card">
+        <h1 class="m-4">Goed te keuren</h1>
+          <div class="m-4 mt-2">
+            <p>POK student 1</p>
+            <p>POK student 2</p>
+            <p>POK student 3</p>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
 @endsection
