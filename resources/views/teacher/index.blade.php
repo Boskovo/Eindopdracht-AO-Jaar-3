73d499e4b15b7c958c9e15ff81d5a0aedb7c3b5e
shanos123 <60260010+shanos123@users.noreply.github.com>
2022-01-24 12:11:04 +0100

    docenten
    scss shit bij gevoegd

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -9,23 +9,49 @@
         
           </div>
           <div class="info_persoon">
+            <ul>
+              <li>Naam en Achtername</li>
+              <li>E-mail</li>
+              <li>Klas</li>
+            </ul>
 
+            
           </div>
-          <div class="CV_block">
-
+          <div class="cv_block">
+            <button type="button">Bekijk CV</button>
           </div>
 
         </div>
       </div>
       <div class="col">
         <div class="container_informatie_stage">
+          <div class="row">
+            <div class="col">
+                <div class="aanvragen_goedkeuring">
+                  <ul>
+                    <li>Stage gevonden:</li>
+                    <li>POK aanvraag:</li>
+                    <li>Andere informatie:</li>
+                  </ul>
+
+
+
+                  <div class="progress">
+                    <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+                  </div>
+                </div>
+                <div class="button_Sollicitatie">
+                </div>
+              </div>
+            </div>
+          </div>
           <div class="huidig-bedrijf">
-          <div class="text">
+            <div class="text">
 
-          </div>
-          <div class="block_company">
+            </div>
+            <div class="block_company">
 
-          </div>
+            </div>
     
           </div>
         </div>
