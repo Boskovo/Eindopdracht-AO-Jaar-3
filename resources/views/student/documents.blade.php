02aa3a11117a2f9805770dc05e23f734f2d55ee9
MarinusGoossens <58777951+MarinusGoossens@users.noreply.github.com>
2022-01-26 14:13:50 +0100

    start documents

--- resources/views/student/documents.blade.php
+++ resources/views/student/documents.blade.php
@@ -3,6 +3,25 @@
 
 @section('content')
 <section class="student-document">
-    <p>test</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="documentdrop">
+                    <h1>Documenten / Werkstaten inleveren</h1>
+                    <form method="post" action="#" id="#">
+                        <div class="file-upload-wrapper">
+                            
+                        </div>
+                        <div class="documentbutton">
+                           <button type="file" class="file-upload">Document Toevoegen</button>
+                        </div>
+                        <div class="inleverenbutton">
+                            <button type="submit" value="submit">inleveren</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
