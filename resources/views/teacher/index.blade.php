7cd6a6ce71759577919a00f6d1b4cbcec9d2a314
shanos123 <60260010+shanos123@users.noreply.github.com>
2022-01-27 10:05:02 +0100

    intern teacher page
    i changed the styling op de pages and add some divs with content

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -16,40 +16,73 @@
                         </ul>
                     </div>
                     <div class="cv_block">
-                        <button type="button">Bekijk CV</button>
+                        <a type="button" class="btn btn-dark">Bekijk CV</a>
                     </div>
 
                 </div>
             </div>
-            <div class="col">
+            <div class="col mt-3 m-lg-0">
                 <div class="container_informatie_stage p-3">
-                    <div class="content-wrapper">
-                        <div class="aanvragen_goedkeuring p-3">
-                            <ul>
-                                <li>Stage gevonden:</li>
-                                <li>POK aanvraag:</li>
-                                <li>Andere informatie:</li>
-                            </ul>
+                    <div class="row">
+                        <div class="col">
+                            <div class="aanvragen_goedkeuring p-3">
+                                <div class="Text-info">
+                                    <ul>
+                                        <li>Stage gevonden:</li>
+                                        <li>POK aanvraag:</li>
+                                        <li>Andere informatie:</li>
+                                    </ul>
+                                </div>
+                                <div class="container_process">
+                                    <div class="progress" style="height: 20px;">
+                                        <div class="progress-bar" role="progressbar" style="width: 25%;"
+                                            aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                                    </div>
+                                    <div class="button-progress">
+                                        <a type="button" class="btn btn-dark">Sollicitaties</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            test
+                        </div>
+                    </div>
 
-                            <div class="progress" style="height: 20px;">
-                                <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row no-pad">
+            <div class="col-12 col-lg-12">
+                <div class="container_ingeleverde_documenten p-3">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="ingeleverd_documenten p-3">
+                                <div>
+                                    (Student) heeft (document) ingeleverd
+                                </div>
+                                <div class="button-progress">
+                                    <a type="button" class="btn btn-dark">Document inzien</a>
+                                </div>
+                                <div>
+                                    (Student) heeft (document) ingeleverd
+                                </div>
+                                <div class="button-progress">
+                                    <a type="button" class="btn btn-dark">Document inzien</a>
+                                </div>
+                                <div>
+                                    (Student) heeft (document) ingeleverd
+                                </div>
+                                <div class="button-progress">
+                                    <a type="button" class="btn btn-dark">Document inzien</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="row no-pad">
-            <div class="col">
-                <div class="container_ingeleverde_documenten">
-    
-                </div>
-            </div>
-        </div>
-
     </div>
-    
 
-
-   
 @endsection
